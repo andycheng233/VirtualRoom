a5892504fb190a799b7b9a86429201462401204f
andycheng233 <andycheng233@gmail.com>
2025-12-13 18:58:26 -0500

    final changes, finished coumentation, edited requirements, other small changes

--- test_read_npy.py
+++ test_read_npy.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 
-depth = np.load("locations/tsai_city/waypoint_1/flat/depth_9.npy")
+depth = np.load("locations/rosenkranz/depth_40.npy")
 
-near = 200     # mm
+'''near = 200     # mm
 far = 10000     # mm
 
 depth_clipped = np.clip(depth, near, far)
@@ -11,10 +11,19 @@ norm = ((depth_clipped - near) / (far - near)) * 255
 norm = norm.astype(np.uint8)
 
 
-color_depth = cv2.applyColorMap(norm, cv2.COLORMAP_JET)
+color_depth = cv2.applyColorMap(norm, cv2.COLORMAP_JET)'''
 
-cv2.imshow("DEPTH COLORMAP", color_depth)
+depth_colormap = cv2.applyColorMap(
+            cv2.convertScaleAbs(depth, alpha=0.03),
+            cv2.COLORMAP_JET
+        )
+        
+
+cv2.imshow("DEPTH COLORMAP", depth_colormap)
 cv2.waitKey(0)
+
+cv2.imwrite("report_images/" + "depth_40" + ".png", depth_colormap)
+
 
 print(depth)
 print(depth.shape)
